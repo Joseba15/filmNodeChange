307cda28c65a153a93f4a4e51d5f68fd8c6e8055
Joseba15 <josebadg15@gmail.com>
2023-06-05 00:09:38 +0200

    falta rutas de productor para completar el sprint

--- controller/film.js
+++ controller/film.js
@@ -11,7 +11,7 @@ const getFilmById = async (req = request,res= response) =>{
     const id = req.params.id;
 
     const film = await Film.findById( id)
-    console.log('film',film);
+    // console.log('film',film);
     if (film == null) {
         return res.status(400).json({
             msg : `Film  doesnt exits`
@@ -22,4 +22,33 @@ const getFilmById = async (req = request,res= response) =>{
 
 }
 
-module.exports = {getFilms,getFilmById}
+const addFilm = async(req, res) => {
+
+    const { title, duration, country, genre,productor } = req.body;
+    
+    const film = new Film({ title, duration, country, genre,productor})
+    const aux = await Film.findById(film.id)
+
+    if (aux == null) {
+        await film.save();
+        res.json( airport)
+        
+    }else{
+        return res.status(400).json({
+            msg : `Film already exits`
+        });
+    }
+
+}
+
+
+const delFilm = async(req = request, res= response) => {
+    const id = req.params.id;
+    if (condition) {
+        
+    }
+    const remove = await Film.findByIdAndRemove(id);
+    res.json(remove)
+}
+
+module.exports = {getFilms,getFilmById,addFilm,delFilm}
